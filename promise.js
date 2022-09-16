dfae0abc2c9ddf11e52672b9fa74d2b98ed689d9
didi-project08 <91584217+didi-project08@users.noreply.github.com>
2022-09-16 14:12:52 +0700

    feat: update promise test2

--- promise.js
+++ promise.js
@@ -11,34 +11,36 @@ const promiseOutput = (param) => {
     (resultIXX === null) ? reject("Data tidak ditemukan !") : resolve(resultIXX)
   })
 
+  let totalIXX = 0
+  let totalVGC = 0
+
   checkPromiseIXX.then((result) => {
     let arrIXX = []
-    let totalIXX = 0
-    let totalVGC = 0
     result.forEach(TheaterIXX = (i, array) => {
       let result = i.hasil
       arrIXX.push(result)
     })
 
     totalIXX = arrIXX.filter(x => x === param).length;
-
-    checkPromiseVGC.then((result) => {
-      let arrVGC = []
-      result.forEach(TheaterVGC = (i, array) => {
-        let result = i.hasil
-        arrVGC.push(result)
-      })
-  
-      totalVGC = arrVGC.filter(x => x === param).length;
-
-      console.log(totalIXX + totalVGC)
-    }).catch((error) => {
-      console.log(error)
-    })
-
   }).catch((error) => {
     console.log(error)
   })
+
+  checkPromiseVGC.then((result) => {
+    let arrVGC = []
+    result.forEach(TheaterVGC = (i, array) => {
+      let result = i.hasil
+      arrVGC.push(result)
+    })
+
+    totalVGC = arrVGC.filter(x => x === param).length;
+  }).catch((error) => {
+    console.log(error)
+  })
+
+  setTimeout(function(){
+    console.log(totalIXX+totalVGC)
+  },300)
 };
 
 module.exports = {
